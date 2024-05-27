d10a1b3461e3706ba52479edd256bccf214d7ce0
HiroK <hiro.kawai16@gmail.com>
2024-05-27 16:01:32 +0900

    フォルダの整理（GirHub上は散らかったまま？）

---
+++ log_in_manager.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+import yaml
+from yaml.loader import SafeLoader
+
+## ユーザー設定読み込み
+yaml_path = "config.yaml"
+
+with open(yaml_path) as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    cookie_key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days'],
+)
+
+## UI 
+authenticator.login()
+if st.session_state["authentication_status"]:
+    ## ログイン成功
+    with st.sidebar:
+        st.markdown(f'## Welcome *{st.session_state["name"]}*')
+        authenticator.logout('Logout', 'sidebar')
+        st.divider()
+    st.write('# ログインしました!')
+    st.page_link("pages/main_.py", label="Home")
+
+elif st.session_state["authentication_status"] is False:
+    ## ログイン成功ログイン失敗
+    st.error('Username/password is incorrect')
+
+elif st.session_state["authentication_status"] is None:
+    ## デフォルト
+    st.warning('Please enter your username and password')
+
+# streamlit run log_in_manager.py    
